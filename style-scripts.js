ef017f6f10a6a274a2721379d214e7ff0f93ee7c
Ben Timor <bent@mint.security>
2025-10-25 18:57:17 +0300

    Improving docs according to requirements

---
+++ style-scripts.js
@@ -0,0 +1,4 @@
+const sidebarContent = document.querySelector('#sidebar-content');
+const borderElement = document.createElement('div');
+borderElement.id = 'custom-border';
+sidebarContent.appendChild(borderElement);
